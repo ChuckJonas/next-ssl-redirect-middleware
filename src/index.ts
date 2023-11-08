5dd0497970b79019e612c8fa864a223dfee54bb0
Karl Becker <karl@karlbecker.com>
2023-11-08 09:16:05 -0600

    Add logging about the environments and current environment
    I've been struggling with Cypress tests in CI - this middleware seems to be running when Cypress runs, and this middleware is forwarding it to https even when running in localhost, which is confusing since the NODE_ENV shouldn't be set to production in that case.  Going to check it out.

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const middleware = ({
     status = 301
 }: Params) => (req: NextRequest, ev: NextFetchEvent) => {
     const currentEnv = process.env.NODE_ENV as Environment;
-
+    console.log('environments', environments, 'currentEnv', currentEnv);
     if (environments.includes(currentEnv) && !req.headers?.get("x-forwarded-proto")?.includes("https")) {
         const hostname = req.headers.get('host') || req.nextUrl.hostname;
         return NextResponse.redirect(`https://${hostname}${req.nextUrl.pathname}`, status);
